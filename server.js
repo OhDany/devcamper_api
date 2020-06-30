2dd3db6a0c13ed5c7471f76df81a6328955922d6
Daniel Ortega <oh.dany.mx@gmail.com>
2020-06-30 12:09:37 -0500

    Add middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Routes files
 const bootcamps = require('./routes/bootcams');
@@ -9,9 +11,16 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+// app.use(logger);
+// Dev loging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
