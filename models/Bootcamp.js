fd76c6785d1b4829b54193ff8519bd86b70edff4
Daniel Ortega <oh.dany.mx@gmail.com>
2020-07-07 15:42:23 -0500

    Populate, virtuals & Cascade Delete

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -98,6 +98,9 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 // Create bootcamp slug from the name
@@ -123,6 +126,21 @@ BootcampSchema.pre('save', async function(next) {
   // Do not save address in BD
   this.address = undefined;
   next();
+});
+
+// Cascade delete courses when a bootcamp i deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
+// Reverse popilate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  jusOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
